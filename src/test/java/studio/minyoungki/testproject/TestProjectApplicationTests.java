ec128136e7c227b63dad7a88542bda23ad58a5b0
minyoungki <aksrkaksen12@gmail.com>
2022-10-04 11:00:13 +0900

    Spring boot create

---
+++ src/test/java/studio/minyoungki/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.minyoungki.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
